c1317a5204829251cf5a3ced819cb37af40b1857
Stassras <stasbarinoff@gmail.com>
2019-11-09 19:47:37 +0300

    feat: do a first task

--- src/components/Task1/Task1.js
+++ src/components/Task1/Task1.js
@@ -3,10 +3,32 @@ import React from "react";
 import "./Task1.css";
 
 export default class Task1 extends React.Component {
+  
+  state = {
+    text: "",
+    isValue: false,
+  };
+
+  initText = "Xxxx";
+
+  inputHandler = e => {
+    const text = e.target.value;
+    this.setState({ text });
+  };
+
   render() {
+    const isValue = this.state.text === this.initText ? "form-control" : "form-control red";
+
     return (
       <div className="task1">
-        <h1>Task 1</h1>
+        <input
+          onChange={this.inputHandler}
+          className={isValue}
+          type="text"
+          value={this.state.text}
+          placeholder={this.initText}
+          id="input-form"
+        />
       </div>
     );
   }
